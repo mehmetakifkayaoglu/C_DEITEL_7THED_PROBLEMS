46655b2973c8a4d296e1f3d4bd1d6ae000bcea3b
Akif <mehmetakifkayaoglu@proton.me>
2026-07-26 00:21:04 +0300

    part3/17.c solved

--- Part3/17.c
+++ Part3/17.c
@@ -1,13 +1,48 @@
+// Start a loop if the account id is not equal to -1
 //
+// ask balance
+// ask spend
+// ask deposit
+// ask credit limit
+//
+// calc is limit enough
+// if limit exceed 
+//      show id
+//      show balance
+//      show limit
+//      say the limit exceed
+//
+// ask account id 
 
 #include <stdio.h>
 
 int main()
 {
     int account_id;
-    
+    int balance, spent, deposit, credit_limit;
+
     printf("Enter account id (-1 to stop) :");
-    sacnf("%d", &account_id);
+    scanf("%d", &account_id);
+
+    while( account_id != -1 )
+    {
+        printf("Enter Start Balance : ");
+        scanf("%d",&balance );
+        printf("Enter Spent : ");
+        scanf("%d",&spent );
+        printf("Enter Deposit : ");
+        scanf("%d",&deposit );
+        printf("Enter Credit Limit : ");
+        scanf("%d",&credit_limit );
+
+        if ( (balance + spent - deposit) > credit_limit)
+        {
+            printf("Account : %d\nCredit Limit : %d\nBalance : %d\nLimit Exceeded", account_id, credit_limit, balance);
+        } 
+
+        printf("Enter account id (-1 to stop) :");
+        scanf("%d", &account_id);
+    }
 
     return 0;
 }
